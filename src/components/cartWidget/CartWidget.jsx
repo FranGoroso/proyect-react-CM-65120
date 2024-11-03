55d4812a73279d2492b6aefc2b68ba3123c2f756
Fran Goroso <gorosofranes@gmail.com>
2024-11-03 14:10:20 +0100

    Add a new function of the cartWidget

--- src/components/cartWidget/CartWidget.jsx
+++ src/components/cartWidget/CartWidget.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../cartWidget/cartWidget.css'
+import { useCart } from '../../context/cartContext'
+
 
 function CartWidget(){
+    const { cart } = useCart()
     return(
         <div className = "cart-widget">
             <span className="cart-icon">🛒</span>
-            <span className="cart-count">1</span>
+            <span className="cart-count">{cart.length}</span>
         </div>
     );
 };
